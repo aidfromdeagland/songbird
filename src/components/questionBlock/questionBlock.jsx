9a920de8adbe894e2268dbecee12aab5dc5de07f
aidfromdeagland <aidfromdeagland@gmail.com>
2020-08-08 16:22:35 +0300

    feat: add icons, previre images, dynamic imports

--- src/components/questionBlock/questionBlock.jsx
+++ src/components/questionBlock/questionBlock.jsx
@@ -7,17 +7,18 @@ import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import '../../themes-change/rhap-theme.css';
 import { connect } from 'react-redux';
-import sourceImage from '../../assets/images/heroes/npc_dota_hero_abaddon_png.png';
 import frameImage from '../../assets/images/hero_hud_sized.png';
-import currentSound from '../../assets/sounds/heroes/abad_death_01.mp3';
+import heroesMap from '../../dota2data/heroesMap';
 
 function QuestionBlock(props) {
   const { heroes } = props;
+  const heroImageUrl = require(`../../assets/images/heroes/selection/${heroes[0].name}_png.png`);
+  const heroAudioUrl = require(`../../assets/sounds/heroes/${heroesMap[heroes[0].name]}_spawn_03.mp3`);
   return (
     <Row className="questionBlock justify-content-center">
       <Col xl={4} lg={4} md={5} sm={6} xs={12}>
         <div className="questionBlock__presentation">
-          <Image className="questionBlock__image" src={sourceImage} alt="current hero" />
+          <Image className="questionBlock__image" src={heroImageUrl} alt="current hero" />
           <Image className="questionBlock__frame" src={frameImage} alt="frame" />
         </div>
       </Col>
@@ -26,7 +27,7 @@ function QuestionBlock(props) {
           <h3 className="text-center">{heroes[0].localized_name}</h3>
           <AudioPlayer
             customAdditionalControls={[]}
-            src={currentSound}
+            src={heroAudioUrl}
           />
         </div>
       </Col>
